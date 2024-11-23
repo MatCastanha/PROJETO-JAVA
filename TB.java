c9636da82bb2a3791f8024755bd4a3f084c12122
Matheus Castanha <matheuscastrod33@gmail.com>
2024-11-23 15:53:39 -0300

    Update TB.java

--- TB.java
+++ TB.java
@@ -132,9 +132,35 @@ public class Main {
         }
     }
 
-    public static void imprimirXMLSAT() {
-        // Função para imprimir XML SAT
+   public static void imprimirXMLSAT() throws java.io.IOException{
+    // Função para imprimir XML SAT
+
+    if (conexaoAberta){
+        try{
+
+            // Nesta variavel será gerada uma nova String apartir do Bytes do arquivo lido
+            String dadosXML = new String(Files.readAllBytes(Paths.get("C:/Users/mathe/Downloads/Projeto Java/XMLSAT.xml")));
+
+            // Fução que irá imprimir o cupom fiscal
+            int retorno = ImpressoraDLL.INSTANCE.ImprimeXMLSAT(dadosXML,0);
+            ImpressoraDLL.INSTANCE.Corte(5);
+
+            //Verificação se a impressão foi feita com sucesso
+            if (retorno==0){
+                System.out.println("XML SAT impresso com sucesso");
+            }
+            else {
+                System.out.println("Erro ao imprimir XML SAT Código: "+ retorno);
+            }
+
+        }catch(Exception e){
+            System.out.println("Erro ao ler ou imprimir o arquivo XML: "+e.getMessage());
+        }
     }
+    else{
+        System.out.println("Erro: Conexão não está aberta");
+    }
+}
 
     public static void imprimirXMLCancelamentoSAT() {
         // Função para imprimir XML de cancelamento SAT
